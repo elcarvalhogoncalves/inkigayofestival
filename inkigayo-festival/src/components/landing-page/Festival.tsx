eb42c57a20151ea9dd83e2bbbc9e113a20590fba
João Paixão <joaopcp23@gmail.com>
2023-07-12 19:55:14 -0300

    Componente de O festival semi concluida (falta o issue dos titulos)

--- inkigayo-festival/src/components/landing-page/Festival.tsx
+++ inkigayo-festival/src/components/landing-page/Festival.tsx
@@ -1,25 +1,36 @@
 import styles from "@/styles/components/landing-page/Festival.module.css";
+import foto from "@/../public/imagens/festival.svg"
+import Image from "next/image";
+
 export default function Festival() {
   return (
-    <>
+    <> 
+    <div className={styles.body} id="#o_festival">
     <div className={styles.texto }>
-      <h1> O Festival</h1>
-      <h1> 제전</h1>
-      <p>
-        O Festival Inkigayo é uma celebração vibrante e emocionante da música
-        coreana, que atrai entusiastas e fãs de K-pop de todo o Nordeste. Sendo
-        reconhecido como o maior festival desse gênero na região. O Inkigayo
-        reúne artistas talentosos e amantes da cultura coreana em uma
+      <div className={styles.title}>
+        <h1> O Festival</h1>
+        <h1> 제전</h1> 
+      </div>
+      <p className={styles.desc}>
+        O  <span className={styles.colortext}>Festival Inkigayo </span> é uma celebração vibrante e emocionante da música
+        coreana, que atrai entusiastas e <span className={styles.colortext}> fãs de K-pop de todo o Nordeste</span>. Sendo
+        reconhecido como o maior festival desse gênero na região. <span className={styles.colortext}>O Inkigayo </span> reúne artistas talentosos e amantes da cultura coreana em uma
         experiência inesquecível.
       </p>
       {/* img de reticiencias */}
-      <p className={styles.quote}>
+      <div className={styles.quote}>
+        <p className={styles.frase}>
         “Lorem ipsum dolor sit amet consectetur. Diam semper est diam nunc
         egestas dui tincidunt sit.”
-      </p>
-      <p className={styles.autor}> Fulano, Ciclano</p>
+        </p>
+        <p className={styles.autor}> Fulano, Ciclano</p>
+        </div>
       </div>
-      {/* img do festival */}
+      
+      <figure className={styles.image}>
+        <Image src={foto} width={630} height={832} />
+      </figure>
+    </div>
     </>
   );
 } 
